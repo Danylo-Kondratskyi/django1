508ebe4b9604166a36e0629dae11ed50e0f62345
LynnuiYogurt <wanosgaming@gmail.com>
2024-02-20 13:22:58 +0200

    2.43

--- main/views.py
+++ main/views.py
@@ -4,16 +4,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	context = {
-	'title': 'Home',
-	'content': 'Furniture Store' }
-	return render(request, "main/index.html", context)
+    context = {
+        'title': 'Home',
+        'content': 'Furniture Store'
+
+    }
+    return render(request, "main/index.html", context)
 
 
 def about(request):
-	context = {
-		'title': 'about',
-		'content': 'About Furniture Store',
-		'page_text': 'This is the about page'
-	}
-	return render(request, "main/about.html", context)
+    context = {
+        'title': 'about',
+        'content': 'About Furniture Store',
+        'page_text': 'This is the about page'
+    }
+    return render(request, "main/about.html", context)
