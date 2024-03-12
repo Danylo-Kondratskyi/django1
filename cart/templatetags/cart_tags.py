2ac93529259fe6af851c0881c43fa8ccc8571995
LynnuiYogurt <wanosgaming@gmail.com>
2024-03-12 22:17:46 +0200

    1.39

--- cart/templatetags/cart_tags.py
+++ cart/templatetags/cart_tags.py
@@ -1,6 +1,7 @@
 from django.utils.http import urlencode
 
 from cart.models import Cart
+from cart.utils import get_user_carts
 from goods.models import Category
 from django import template
 
@@ -9,7 +10,4 @@ register = template.Library()
 
 @register.simple_tag()
 def user_carts(request):
-    if request.user.is_authenticated:
-        return Cart.objects.filter(user=request.user)
-    else:
-        return Cart.objects.none()
+    return get_user_carts(request)
--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,9 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
 
 from cart.models import Cart
+from cart.utils import get_user_carts
 from goods.models import Products
 
 
@@ -8,8 +11,9 @@ def cart_change(request, product_slug):
     return render(request, "cart/cart_change.html")
 
 
-def cart_add(request, product_slug):
-    product = Products.objects.get(slug=product_slug)
+def cart_add(request):
+    product_id = request.POST.get("product_id")
+    product = Products.objects.get(id=product_id)
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
 
@@ -21,8 +25,33 @@ def cart_add(request, product_slug):
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
-    return redirect(request.META["HTTP_REFERER"])
+    user_cart = get_user_carts(request)
+
+    cart_items_html = render_to_string(
+        "cart/includes/include_cart.html", {"carts": user_cart}, request=request)
+
+    response_data = {
+        "message": "Product added to cart",
+        "cart_items_html": cart_items_html,
+    }
+
+    return JsonResponse(response_data)
 
 
-def cart_remove(request, product_slug):
-    return render(request, "cart/cart_remove.html")
+def cart_remove(request):
+    cart_id = request.POST.get("cart_id")
+    cart = Cart.objects.get(id=cart_id)
+    quantity = cart.quantity
+    cart.delete()
+    user_cart = get_user_carts(request)
+
+    cart_items_html = render_to_string(
+        "cart/includes/include_cart.html", {"carts": user_cart}, request=request)
+
+    response_data = {
+        "message": "Product removed from cart",
+        "cart_items_html": cart_items_html,
+        "quantity": quantity,
+    }
+
+    return JsonResponse(response_data)
