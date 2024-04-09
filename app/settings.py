ba1f0b2aa676108610620c8477b9dd885da4de9a
LynnuiYogurt <wanosgaming@gmail.com>
2024-04-09 14:13:05 +0300

    change db url

--- app/settings.py
+++ app/settings.py
@@ -93,8 +93,7 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 
 DATABASES = {
-    "default": dj_database_url.parse(env("DATABASE_URL"))
-
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 # Password validation
