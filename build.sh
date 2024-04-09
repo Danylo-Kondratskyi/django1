547e6056231646e1eb47748b89dcf346fcb152ce
LynnuiYogurt <wanosgaming@gmail.com>
2024-04-09 13:56:18 +0300

    change build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+# Frontend setup
+npm install                # Install Node.js dependencies (if you have a package.json)
+npm run build              # Build the JavaScript assets
