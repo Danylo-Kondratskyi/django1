f2dd7d0794134fe7481cebd311e90d8d66510ccc
LynnuiYogurt <wanosgaming@gmail.com>
2024-04-10 17:00:58 +0300

    change settings.py

--- app/settings.py
+++ app/settings.py
@@ -44,9 +44,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.postgres",
-
-    "debug_toolbar",
-
     "main",
     "goods",
     "users",
@@ -64,8 +61,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "app.urls"
